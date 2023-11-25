f5f366ff22c3ec2193a8c7a5cf6441c3cbbdb7ef
GabrielleMrRobot <gbrizuelag@gmail.com>
2023-11-25 17:43:20 -0400

    feat: finish main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+//creacion dell jquery
+$(document).ready(function() {
+    //Hacer dos funciones que agreguen y remuevan la clase en la etiqueta span
+    function add() {
+        $(".copied").addClass("bounce-effect");
+    }
+    function remove() {
+        $(".copied").removeClass("bounce-effect");
+    }
+
+    //Llamamos a las funciones y copiamos el texto con el click en el boton
+    $(".copy-btn").click(function () {
+        $("#texField").select();
+        document.execCommand("copy");
+        add();
+        setTimeout(remove, 800);
+    });
+});
